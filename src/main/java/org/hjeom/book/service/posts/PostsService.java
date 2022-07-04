aff2b83fe800600936190f74c33a040927bbf8a5
hjeom <nara1030@naver.com>
2022-07-05 01:49:24 +0900

    feat] 등록 API 작성

---
+++ src/main/java/org/hjeom/book/service/posts/PostsService.java
@@ -0,0 +1,18 @@
+package org.hjeom.book.service.posts;
+
+import lombok.RequiredArgsConstructor;
+import org.hjeom.book.domain.posts.PostsRepository;
+import org.hjeom.book.web.dto.PostsSaveRequestDto;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@RequiredArgsConstructor
+@Service
+public class PostsService {
+    private final PostsRepository postsRepository;
+
+    @Transactional
+    public Long save(PostsSaveRequestDto requestDto) {
+        return postsRepository.save(requestDto.toEntity()).getId();
+    }
+}
