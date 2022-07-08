a240497a77522ced4bc1ba72bcfecc7ff1b354f5
hjeom <nara1030@naver.com>
2022-07-08 12:23:44 +0900

    feat] 조회 페이지 수정 시각 노출

--- src/main/java/org/hjeom/book/web/dto/PostsListResponseDto.java
+++ src/main/java/org/hjeom/book/web/dto/PostsListResponseDto.java
@@ -3,15 +3,19 @@ package org.hjeom.book.web.dto;
 import lombok.Getter;
 import org.hjeom.book.domain.posts.Posts;
 
+import java.time.LocalDateTime;
+
 @Getter
 public class PostsListResponseDto {
     private Long id;
     private String title;
     private String author;
+    private LocalDateTime modifiedDateTime;
 
     public PostsListResponseDto(Posts entity) {
         this.id = entity.getId();
         this.title = entity.getTitle();
         this.author = entity.getAuthor();
+        this.modifiedDateTime = entity.getModifiedDateTime();
     }
 }
